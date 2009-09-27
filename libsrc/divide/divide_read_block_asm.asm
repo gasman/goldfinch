aedc5cc57439d3b6be154deafc89eae0bbdf14e9
Matt Westcott <matt@west.co.tt>
2009-09-27 16:27:49 +0100

    Apply bounce detection on DivIDE read (thanks Velesoft)

--- libsrc/divide/divide_read_block_asm.asm
+++ libsrc/divide/divide_read_block_asm.asm
@@ -8,8 +8,14 @@ include	"divide.def"
 	;         bcde = LBA sector number
 	;         ix = pointer to BLOCK_DEVICE structure
 	; exit  : carry set if successful
-	; uses  : af, bc, de, hl
+	; uses  : af, bc, de, hl, af'
 .divide_read_block_asm
+	ld a,20	; attempt read up to 20 times
+.retry
+	ex af,af'	;'
+	push de
+	push bc
+
 	; get drive ID byte (contributes bits 4 and 6 of the "LBA bits 24..28 + drive" register)
 	ld a,(ix + divide_blockdev_driveid)
 	or b
@@ -37,9 +43,26 @@ include	"divide.def"
 	and 0x10	; if bit 4 set, error occurred
 	ret nz	; exit with carry reset
 	
-	ld bc,0x00a3	; C = data register, 0x00 = loop x256
-	inir	; inir twice to read 512 bytes
-	inir
+	ld bc,0x00a3 ; C = data register, 0x00 = loop x256
+	inir ; read first 256 bytes
+	dec b
+	dec b
+	inir ; read only 254 bytes
+	in a,(0xbf) ; test read error before read last word form IDE
+	ini
+	ini ;read complette sector
+	bit 3,a ;if DRQ=0 then sector is read with errors (bouncing - early read sector data)
 
+	pop bc
+	pop de
+	jr nz,success
+	; program must reread same IDE sector if error is detected
+	ex af,af'	;'
+	dec a	; decrement retry counter
+	jr nz,retry
+	; too many failures; signal error
+	or a	; reset carry flag
+	ret
+.success
 	scf	; indicate success
 	ret
